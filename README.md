9434a2a0ded97ed811f8ff3c1ec1980bb0d7171f
Jon3sjns <silendprojeck@gmail.com>
2023-11-29 10:02:48 +0700

    Dir scanner

---
+++ README.md
@@ -0,0 +1,23 @@
+# <p align="center">NoDirScanner</p>
+![carbon (10)](https://github.com/ICWR-TEAM/nodirScanner/assets/45759837/dc94801e-63ca-4325-a136-ddbaf0391eda)
+
+<p>This tool is used to help you, one of which is in the field of hacking, such as scanning sensitive files.</p>
+
+# Requirement
+
+```
+1. Internet Connection
+2. Node.js
+```
+
+# How To Use??
+
+```
+1. Download this repo
+2. Run node scanner.js
+3. Enter your target url(use http or https)
+4. And wait for the results
+```
+
+# License
+![68747470733a2f2f696d672e736869656c64732e696f2f6769746875622f6c6963656e73652f4e61657265656e2f5374726170446f776e2e6a732e737667](https://github.com/ICWR-TEAM/nodirScanner/assets/45759837/b97773d3-68f9-43ea-813b-f22ccfeb698a)
---
+++ scanner.js
@@ -0,0 +1,91 @@
+const prompt = require("prompt-sync")();
+const req = require("request");
+const fs = require("fs");
+const url = require("url");
+const dir = fs.readFileSync("./wl.txt","utf8"); //change Wordlist
+const split_dir = dir.split("\n");
+
+console.log("=====================================")
+console.log("              DirBuster              ")
+console.log("=====================================")
+
+
+const link = prompt("URL(Use http or https): ");
+function anjay(x, url){
+    return new Promise((resolve) =>{
+        try {
+            const ua = [
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+                "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12",
+                "Mozilla/5.0 (iPad; CPU OS 8_4 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H143 Safari/600.1.4",
+                "Mozilla/5.0 (iPad; CPU OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F69 Safari/600.1.4",
+                "Mozilla/5.0 (Windows NT 6.1; rv:40.0) Gecko/20100101 Firefox/40.0",
+                "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)",
+                "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)",
+                "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko",
+                "Mozilla/5.0 (Windows NT 5.1; rv:40.0) Gecko/20100101 Firefox/40.0",
+                "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.6.3 (KHTML, like Gecko) Version/8.0.6 Safari/600.6.3",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.5.17 (KHTML, like Gecko) Version/8.0.5 Safari/600.5.17",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+                "Mozilla/5.0 (iPad; CPU OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53",
+                "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:40.0) Gecko/20100101 Firefox/40.0",
+                "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)"
+            ]
+            const uaRand = ()=>{
+                const rand = Math.floor(Math.random()*(ua.length-0))+0
+                if (rand < 0){
+                    return "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)"
+                }else{
+                    return ua[rand]
+                }
+            }
+            const options = {
+                headers: {
+                    'User-Agent': uaRand()
+                }
+            }
+            const q = req(url+"/"+x, options, async function(error, response){
+                try{
+                    const cok = await response.statusCode;
+                    if (cok == "200") {
+                        console.log("\r"+cok + " ===> "+url+"/"+x);
+                    }
+                    else{
+                        var twirlTimer = (function() {
+                          var P = ["[\\]", "[|]", "[/]", "[-]"];
+                          var x = 0;
+                          return setInterval(function() {
+                            process.stdout.write("\r" + P[x++]+" Loading");
+                            x &= 3;
+                        }, 250);
+                        })();
+                    }
+                    resolve(cok);
+                }catch(e){
+                    resolve(null);
+                }
+            }).setMaxListeners(0);
+        } catch (e) {
+            resolve(null);
+        }
+    })
+}
+async function main(url){
+    for(let i of split_dir){
+        const res = await anjay(i,url);
+        res;
+    }
+}
+console.log("Result:")
+main(link)
